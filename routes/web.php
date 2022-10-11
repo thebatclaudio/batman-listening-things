a7135057535fa80f5fb31c8eb12045e860b023d5
Claudio La Barbera <claudio.labarbera@myfamilyplace.com>
2022-10-11 10:58:33 +0200

    added telegram bot

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', "App\Http\Controllers\VideoController@homepage");
 Route::post('/generate', "App\Http\Controllers\VideoController@generateBatmanVideo")->name("generate");
+
+Route::get('/test', function () {
+    $response = Telegram::getMe();
+
+    $botId = $response->getId();
+    $firstName = $response->getFirstName();
+    $username = $response->getUsername();
+
+    dd(compact("botId", "firstName", "username"));
+});
