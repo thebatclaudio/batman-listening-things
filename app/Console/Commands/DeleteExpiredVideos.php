475b404a729dedaf56594c02e66f1a6a52393ae9
Magnaibayar <magnaibayar.pda@gmail.com>
2022-10-14 02:38:12 +0800

    made hours argument as optional

--- app/Console/Commands/DeleteExpiredVideos.php
+++ app/Console/Commands/DeleteExpiredVideos.php
@@ -14,7 +14,7 @@ class DeleteExpiredVideos extends Command
      *
      * @var string
      */
-    protected $signature = 'videos:delete {hours} {prefix?}';
+    protected $signature = 'videos:delete {hours?} {prefix?}';
 
     private $hours;
 
